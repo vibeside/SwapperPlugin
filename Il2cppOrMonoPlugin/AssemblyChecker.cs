8a37203c2371b723f437cae83aa144ba981c2131
vibeside <vibeside01@gmail.com>
2025-04-21 15:33:25 -0400

    Add project files.

---
+++ Il2cppOrMonoPlugin/AssemblyChecker.cs
@@ -0,0 +1,43 @@
+﻿using MelonLoader;
+using Mono.Cecil;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SwapperPlugin
+{
+    enum Backend
+    {
+        Unknown,
+        Il2cpp,
+        Mono
+    }
+    internal class AssemblyChecker
+    {
+        public static string[] monoRefs = ["mscorlib","netstandard","scheduleone","system"];
+        public static string[] il2cppRefs = ["il2cppmscorlib","il2cppscheduleone","il2cppsystem"];
+        public static Backend CheckAtPath(string path)
+        {
+            string workingDir = Path.GetDirectoryName(path);
+            if (!File.Exists(path)) return Backend.Unknown;
+            if (!path.Contains(".dll")) return Backend.Unknown;
+            AssemblyDefinition def = AssemblyDefinition.ReadAssembly(path);
+            if (Path.GetFileName(path).ToLower().Contains("mono")) return Backend.Mono;
+            if(Path.GetFileName(path).ToLower().Contains("il2cpp")) return Backend.Il2cpp;
+            foreach (ModuleDefinition m in def.Modules)
+            {
+                foreach (TypeReference s in m.GetTypeReferences())
+                {
+                    if (!string.IsNullOrEmpty(s.Namespace))
+                    {
+                        if (il2cppRefs.Contains(s.Namespace.ToLower())) return Backend.Il2cpp;
+                        if (monoRefs.Contains(s.Namespace.ToLower()) && !s.Namespace.ToLower().Contains("il2cpp"))
+                            return Backend.Mono;
+                    
+                    }
+                }
+            }
+            return Backend.Unknown;
+        }
+    }
+}
---
+++ Il2cppOrMonoPlugin/Plugin.cs
@@ -0,0 +1,50 @@
+﻿using MelonLoader;
+using MelonLoader.Utils;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+[assembly: MelonInfo(typeof(SwapperPlugin.Plugin), "SwapperPlugin", "1.0.0", "coolpaca")]
+[assembly:MelonColor(255,149,82,255)]
+namespace SwapperPlugin
+{
+    public class Plugin : MelonPlugin
+    {
+        static Backend backend = Backend.Unknown;
+        public override void OnPreInitialization()
+        {
+            base.OnPreInitialization();
+            string mods = Path.GetDirectoryName(MelonEnvironment.ModsDirectory) + @"\Mods";
+            //MelonLogger.Msg(mods);
+            backend = MelonUtils.IsGameIl2Cpp() ? Backend.Il2cpp : Backend.Mono;
+            if (Directory.Exists(mods))
+            {
+                string[] files = Directory.GetFiles(mods, "*", SearchOption.AllDirectories);
+                Backend fBackend;
+                for(int i = 0; i < files.Length; i++) {
+                    if (files[i].Contains(".dll"))
+                    {
+                        File.Move(files[i], files[i].Replace(".di", ""));
+                        files[i] = files[i].Replace(".di", "");
+                    }
+                }
+                foreach (string s in files)
+                {
+                    if (!s.Contains(".dll")) continue;
+                    if (s.Contains(".old")) continue;
+                    if (Path.GetFileName(s).Contains("S1API")) continue;
+                    fBackend = AssemblyChecker.CheckAtPath(s);
+                    //MelonLogger.Msg(fBackend.ToString());
+                    if (fBackend == Backend.Unknown) MelonLogger.Error(
+                        "Found a DLL, but couldn't determine backend!" +
+                        "\nTell coolpaca in the S1 modding discord and" +
+                        $"\nprovide the mod name! File: {Path.GetFileName(s)}");
+                    if (fBackend == backend) continue;
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    File.Move(s, s + ".di");
+                }
+            }
+        }
+    }
+}
